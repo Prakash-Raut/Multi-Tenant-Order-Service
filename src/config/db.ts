fecbdc19d20608286092e3a449b10ffdb03abb47
Prakash Raut <prakash_raut@outlook.com>
2025-02-12 16:22:14 +0530

    feat: add database connection logic with mongoose and config

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import config from "config";
+import { connect } from "mongoose";
+import logger from "./logger";
+
+export const dbConnect = async () => {
+  try {
+    await connect(config.get("database.url"));
+    logger.info("Database connected successfully!");
+  } catch (error) {
+    logger.error("Database connection failed!", error);
+    process.exit(1);
+  }
+};
