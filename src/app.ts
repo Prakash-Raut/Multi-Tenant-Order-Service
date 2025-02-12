6890bc29c3d9ece27f57f631f24267cfdf008993
Prakash Raut <prakash_raut@outlook.com>
2025-02-12 16:32:51 +0530

    feat: add express app with middleware

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import config from "config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(
+	cors({
+		origin: config.get("client.url"),
+		credentials: true,
+	})
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+	res.json({ message: config.get("server.port") });
+});
+
+// app.use(globalErrorHandler);
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import config from "config";
+import app from "./app";
+import { dbConnect } from "./config/db";
+import logger from "./config/logger";
+
+const startServer = async () => {
+	const PORT: number = config.get("server.port");
+	try {
+		await dbConnect();
+		app.listen(PORT, () => {
+			logger.info("Server listening on port", { port: PORT });
+		});
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			logger.error(err.message);
+			setTimeout(() => {
+				process.exit(1);
+			}, 1000);
+		}
+	}
+};
+
+void startServer();
